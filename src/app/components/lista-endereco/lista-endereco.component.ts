b5cfa5efe600e07addc7bf47eb30f1e9b685d06b
Jardel <dell_machado@yahoo.com.br>
2024-05-31 20:12:56 -0300

    Vídeo 368 - ajusta o método prepararListaEnderecoParaMostrar

--- src/app/components/lista-endereco/lista-endereco.component.ts
+++ src/app/components/lista-endereco/lista-endereco.component.ts
@@ -4,6 +4,7 @@ import { TipoEnderecoEnum } from '../../enums/tipo-endereco.enum';
 import { IEndereco } from '../../interfaces/usuario/endereco.interface';
 import { IEnderecoParaMostrar } from '../../interfaces/endereco-para-mostrar.interface';
 import { descricaoTipoEnderecoMap } from '../../utils/descricao-tipo-endereco-map';
+import { prepararListaEndereco } from '../../utils/preparar-lista-endereco';
 
 @Component({
   selector: 'app-lista-endereco',
@@ -27,6 +28,12 @@ export class ListaEnderecoComponent implements OnChanges {
   }
   prepararListaEnderecoParaMostrar() {
     this.listaEnderecoParaMostrar = [];    
+
+    const listaOriginalEnderecoUsuario = this.listaEnderecoUsuario && this.listaEnderecoUsuario.length > 0 ? this.listaEnderecoUsuario : [];
+
+    prepararListaEndereco(listaOriginalEnderecoUsuario, true, (endereco) => {
+      this.listaEnderecoParaMostrar.push(endereco);
+    });
   }
   
 }
