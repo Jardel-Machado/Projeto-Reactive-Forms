80ffec8fb7dd83bbdbc475a39b2a028053921d0b
Jardel <dell_machado@yahoo.com.br>
2024-05-31 20:49:33 -0300

    Vídeo 370 - Renderização da lista de endereços para editar.

--- src/app/components/lista-endereco-editar/lista-endereco-editar.component.ts
+++ src/app/components/lista-endereco-editar/lista-endereco-editar.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormArray, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-lista-endereco-editar',
   templateUrl: './lista-endereco-editar.component.html',
-  styleUrl: './lista-endereco-editar.component.scss'
+  styleUrl: './lista-endereco-editar.component.scss',
 })
 export class ListaEnderecoEditarComponent {
+  
+  @Input({ required: true }) usuarioForm!: FormGroup;
 
+  get listaEndereco(): FormArray {
+    return this.usuarioForm.get('informacoesContato.listaDeEnderecos') as FormArray;
+  }
 }
