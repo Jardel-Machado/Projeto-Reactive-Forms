0ffb0fc3ad2b8bd093a9ba126cbd81e69af6eab7
Jardel <dell_machado@yahoo.com.br>
2024-06-01 00:32:54 -0300

    Vídeo 377 - Cria lógica para desabilitar usuários.

--- src/app/components/lista-de-usuarios/lista-de-usuarios.component.ts
+++ src/app/components/lista-de-usuarios/lista-de-usuarios.component.ts
@@ -11,10 +11,12 @@ export class ListaDeUsuariosComponent {
   indexUsuarioSelecionado: number | undefined;
 
   @Input({ required: true }) listaDeUsuarios: ListaDeUsuariosResponse = [];
+  @Input({ required: true }) modoEdicao: boolean = false;
   
   @Output('usuarioSelecionado') usuarioSelecionadoEmitt = new EventEmitter<number>();
 
   usuarioSelecionado(indexUsuario: number) {
+    if(this.modoEdicao) return;
     this.indexUsuarioSelecionado = indexUsuario;
     this.usuarioSelecionadoEmitt.emit(indexUsuario);
   }
