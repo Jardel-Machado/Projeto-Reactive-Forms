50c22d5c3e4be0e73a5b8865a644578670a7f823
Jardel <dell_machado@yahoo.com.br>
2024-05-31 23:29:17 -0300

    Video 373 e 374 - Renderizar lista de dependentes - Mensagens de erro. - Mascara de documento.

--- src/app/components/lista-dependentes-editar/lista-dependentes-editar.component.ts
+++ src/app/components/lista-dependentes-editar/lista-dependentes-editar.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormArray, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-lista-dependentes-editar',
   templateUrl: './lista-dependentes-editar.component.html',
-  styleUrl: './lista-dependentes-editar.component.scss'
+  styleUrl: './lista-dependentes-editar.component.scss',
 })
 export class ListaDependentesEditarComponent {
+  @Input({ required: true }) usuarioForm!: FormGroup;
 
+  get listaDependente(): FormArray {
+    return this.usuarioForm.get('listaDeDependentes') as FormArray;
+  }
 }
